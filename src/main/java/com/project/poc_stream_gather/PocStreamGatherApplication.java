86f5e75fd737423ed935c31eabb2d951b4ec4bdf
ICAROCAETANO <icaro.caetano@gmail.com>
2026-06-16 23:13:30 -0300

    first commit

---
+++ src/main/java/com/project/poc_stream_gather/PocStreamGatherApplication.java
@@ -0,0 +1,13 @@
+package com.project.poc_stream_gather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocStreamGatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocStreamGatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poc_stream_gather/PocStreamGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poc_stream_gather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocStreamGatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
